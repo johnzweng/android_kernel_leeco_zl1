070165c0e9d5378b29ee1ea489c0427349965985
hondajohn88 <hondajohn88@gmail.com>
2016-11-24 17:40:11 -0800

    2nd try at the zl1 kernel tree.

---
+++ out/usr/include/uapi/..install.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/uapi/.install := /bin/bash ../scripts/headers_install.sh ./usr/include/uapi ../include/uapi ; /bin/bash ../scripts/headers_install.sh ./usr/include/uapi ../include ; /bin/bash ../scripts/headers_install.sh ./usr/include/uapi ./include/generated/uapi ; for F in ; do echo "\#include <asm-generic/$$F>" > ./usr/include/uapi/$$F; done; touch usr/include/uapi/.install
